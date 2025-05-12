a519c3ec7acf04e0be8f6a120c80adaf7cbe8455
objektwerks <objektwerks@runbox.com>
2025-05-12 14:00:08 -0400

    enhanced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -3,16 +3,17 @@ package objektwerks
 import plotly.Scatter
 import plotly.Plotly
 import plotly.layout.Layout
+import plotly.Config
 
 @main def runPlotly: Unit =
   val line1 = Scatter(
-    Seq(1, 2, 3, 4),
-    Seq(10, 15, 13, 17)
+    x = Seq(1, 2, 3, 4),
+    y = Seq(10, 15, 13, 17)
   )
   
   val line2 = Scatter(
-    Seq(1, 2, 3, 4),
-    Seq(16, 5, 11, 9)
+    x = Seq(1, 2, 3, 4),
+    y = Seq(16, 5, 11, 9)
   )
   
   val data = Seq(line1, line2)
@@ -20,5 +21,14 @@ import plotly.layout.Layout
   val layout = Layout(
     title = "Line Chart"
   )
+
+  val config = Config(
+    responsive = true
+  )
     
-  Plotly.plot("plot", data, layout)
+  Plotly.plot(
+    div = "plot",
+    data = data,
+    layout = layout,
+    config = config
+  )
