d8671b53c0135e019b140e95b178f7fbd72fed28
objektwerks <objektwerks@runbox.com>
2025-05-12 10:38:49 -0400

    refactored app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,17 +1,19 @@
 package objektwerks
 
 import plotly.Scatter
-import plotly.layout.Layout
-import plotly.Plotly.TraceSeqOps
+import plotly.Plotly
 
 @main def runPlotly: Unit =
-  val x = (0 to 100).map(_ * 0.1)
-  val y1 = x.map(d => 2.0 * d + util.Random.nextGaussian())
-  val y2 = x.map(math.exp)
-  
-  val plot = Seq(
-    Scatter(x, y1).withName("Approx twice"),
-    Scatter(x, y2).withName("Exp")
+  val trace1 = Scatter(
+    Seq(1, 2, 3, 4),
+    Seq(10, 15, 13, 17)
   )
-  val layout = Layout().withTitle("Curves")
-  plot.plot(div = "plot", layout = layout)
+  
+  val trace2 = Scatter(
+    Seq(1, 2, 3, 4),
+    Seq(16, 5, 11, 9)
+  )
+  
+  val data = Seq(trace1, trace2)
+  
+  Plotly.plot("plot", data)
