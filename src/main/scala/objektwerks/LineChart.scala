7fe935977453253043a8abbaad77cb0185fea84b
objektwerks <objektwerks@runbox.com>
2025-05-12 14:44:51 -0400

    enhanced line chart

--- src/main/scala/objektwerks/LineChart.scala
+++ src/main/scala/objektwerks/LineChart.scala
@@ -9,20 +9,19 @@ object LineChart:
     val line1 = Scatter(
       x = Seq(1, 2, 3, 4),
       y = Seq(3, 5, 7, 9)
-    )
+    ).withName("line 1")
 
     val line2 = Scatter(
       x = Seq(1, 2, 3, 4),
       y = Seq(2, 4, 6, 8)
-    )
+    ).withName("line 2")
     
     val data = Seq(line1, line2)
   
     val layout = Layout(
-      title = "Line Chart",
-      xaxis = Axis(title = "line 1"),
-      yaxis = Axis(title = "line 2")
-    )
+    ).withTitle("Line Chart")
+     .withXaxis(Axis(title = "line 1"))
+     .withYaxis(Axis(title = "line 2"))
   
     val config = Config(
       responsive = true
