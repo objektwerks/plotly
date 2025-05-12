554a35e56ac40418a04d9fd7a167765323b5682f
objektwerks <objektwerks@runbox.com>
2025-05-12 16:03:53 -0400

    enhanced line chart

--- src/main/scala/objektwerks/LineChart.scala
+++ src/main/scala/objektwerks/LineChart.scala
@@ -5,17 +5,17 @@ import plotly.layout.Layout
 
 object LineChart: 
   def apply(id: String): Unit =
-    val line1 = Scatter(
+    val series1 = Scatter(
       x = Seq(1, 2, 3, 4),
       y = Seq(3, 5, 7, 9)
-    ).withName("line 1")
+    ).withName("series 1")
 
-    val line2 = Scatter(
+    val series2 = Scatter(
       x = Seq(1, 2, 3, 4),
       y = Seq(4, 6, 8, 10)
-    ).withName("line 2")
+    ).withName("series 2")
     
-    val data = Seq(line1, line2)
+    val data = Seq(series1, series2)
   
     val layout = Layout().withTitle("Line Chart")
   
