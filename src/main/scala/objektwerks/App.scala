a10d1785d3b4af7ba2be52a353905a8ed476b519
objektwerks <objektwerks@runbox.com>
2025-05-12 09:53:14 -0400

    refactored app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -4,15 +4,14 @@ import org.scalajs.dom
 import org.scalajs.dom.document
 import org.scalajs.dom.HTMLDocument
 
-object App:
-  def main(args: Array[String]): Unit =
-    document.addEventListener("DOMContentLoaded", { (_: dom.Event) =>
-      build(document)
-    })
+@main def runPlotly: Unit =
+  document.addEventListener("DOMContentLoaded", { (_: dom.Event) =>
+    build(document)
+  })
 
-  def build(document: HTMLDocument): Unit =
-    val paragraph = document.createElement("p")
-    paragraph.id = "pid"
-    paragraph.textContent = "Plotly Web App!"
-    document.body.appendChild(paragraph)
-    ()
+def build(document: HTMLDocument): Unit =
+  val paragraph = document.createElement("p")
+  paragraph.id = "pid"
+  paragraph.textContent = "Plotly Web App!"
+  document.body.appendChild(paragraph)
+  ()
