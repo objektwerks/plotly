8a0d70a24898652517bdf020ea62f59422214e42
objektwerks <objektwerks@runbox.com>
2025-05-12 10:08:03 -0400

    added plot to app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -4,6 +4,8 @@ import org.scalajs.dom
 import org.scalajs.dom.document
 import org.scalajs.dom.HTMLDocument
 
+import plotly.*, layout.*, Plotly.*
+
 @main def runPlotly: Unit =
   document.addEventListener("DOMContentLoaded", { (_: dom.Event) =>
     build(document)
@@ -18,4 +20,16 @@ def build(document: HTMLDocument): Unit =
   val div = document.createElement("div")
   div.id = "plot"
   document.body.appendChild(div)
+
+  val x = (0 to 100).map(_ * 0.1)
+  val y1 = x.map(d => 2.0 * d + util.Random.nextGaussian())
+  val y2 = x.map(math.exp)
+  
+  val plot = Seq(
+    Scatter(x, y1).withName("Approx twice"),
+    Scatter(x, y2).withName("Exp")
+  )
+  val layout = Layout().withTitle("Curves")
+  plot.plot(div = "plot", layout = layout)
+
   ()
