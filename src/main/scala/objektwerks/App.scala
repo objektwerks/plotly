62d59285e2a2369c15c6dc9af3bc229ca58af010
objektwerks <objektwerks@runbox.com>
2025-05-12 10:25:48 -0400

    refactored app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,28 +1,10 @@
 package objektwerks
 
-import org.scalajs.dom
-import org.scalajs.dom.document
-import org.scalajs.dom.HTMLDocument
-
 import plotly.Scatter
 import plotly.layout.Layout
 import plotly.Plotly.TraceSeqOps
 
 @main def runPlotly: Unit =
-  document.addEventListener("DOMContentLoaded", { (_: dom.Event) =>
-    build(document)
-  })
-
-def build(document: HTMLDocument): Unit =
-  val paragraph = document.createElement("p")
-  paragraph.id = "pid"
-  paragraph.textContent = "Plotly Web App!"
-  document.body.appendChild(paragraph)
-
-  val div = document.createElement("div")
-  div.id = "plot"
-  document.body.appendChild(div)
-
   val x = (0 to 100).map(_ * 0.1)
   val y1 = x.map(d => 2.0 * d + util.Random.nextGaussian())
   val y2 = x.map(math.exp)
