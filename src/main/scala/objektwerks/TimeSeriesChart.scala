043028f4f0baa348fbcb125e57a93b4b8a5e12eb
objektwerks <objektwerks@runbox.com>
2025-05-13 05:45:02 -0400

    refactored time series chart

--- src/main/scala/objektwerks/TimeSeriesChart.scala
+++ src/main/scala/objektwerks/TimeSeriesChart.scala
@@ -5,12 +5,12 @@ import plotly.layout.Layout
 
 object TimeSeriesChart:
   def apply(id: String): Unit =
-    val data = Seq(
-      Scatter(
-        Seq("2025-05-10", "2025-05-11", "2025-05-12"),
-        Seq(1, 3, 6)
-      )
+    val series = Scatter(
+      Seq("2025-05-10", "2025-05-11", "2025-05-12"),
+      Seq(1, 3, 6)
     )
+
+    val data = Seq(series)
 
     val layout = Layout().withTitle("Time Series Chart")
   
